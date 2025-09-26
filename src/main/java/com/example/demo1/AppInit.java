4efd1c04da4e8ab5d640092b5a4619176055a2bb
Le-Hoang-Tien <tienlehoanghn2005@gmail.com>
2025-09-26 20:39:24 +0700

    update

--- src/main/java/com/example/demo1/AppInit.java
+++ src/main/java/com/example/demo1/AppInit.java
@@ -12,7 +12,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] {WebConfig.class};
+        return new Class<?>[] {WebConfig.class,ThymeleafConfig.class};
     }
 
     @Override
